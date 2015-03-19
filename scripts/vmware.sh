87e28e4f939b8ee2061d41e5a7dcfe7b737b0bf7
Akihiro Uchida <uchida@turbare.net>
2015-03-19 10:29:23 +0900

    use open-vm-tools-nox11 and setup with sysrc for vmware

--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
 set -ex
-
-mkdir /tmp/vmfusion
-mkdir /tmp/vmfusion-archive
-
-mdconfig -a -t vnode -f freebsd.iso -u 0
-mount -t cd9660 /dev/md0 /tmp/vmfusion
-tar xf /tmp/vmfusion/vmware-freebsd-tools.tar.gz -C /tmp/vmfusion-archive
-umount /tmp/vmfusion
-rm -rf /tmp/vmfusion
-
-pkg install -y perl5 compat6x-`uname -m`
-/tmp/vmfusion-archive/vmware-tools-distrib/vmware-install.pl --default
-rm -rf /tmp/vmfusion-archive
-rm freebsd.iso
-
-echo 'ifconfig_vxn0="dhcp"' >> /etc/rc.conf
+pkg install -y open-vm-tools-nox11
+sysrc ifconfig_vxn0="dhcp"
+sysrc vmware_guest_vmblock_enable="YES"
+sysrc vmware_guest_vmhgfs_enable="YES"
+sysrc vmware_guest_vmmemctl_enable="YES"
+sysrc vmware_guest_vmxnet_enable="YES"
+sysrc vmware_guestd_enable="YES"
