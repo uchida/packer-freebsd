b2a771d94d9ad10dcf63a1065a92a8727e2b8bbd
Akihiro Uchida <uchida@turbare.net>
2015-04-29 04:00:46 +0900

    put freebsd-update subcommands together

--- scripts/base.sh
+++ scripts/base.sh
@@ -3,8 +3,8 @@ set -ex
 
 sed -e 's/\[ ! -t 0 \]/false/' /usr/sbin/freebsd-update > /tmp/freebsd-update
 chmod +x /tmp/freebsd-update
-env PAGER=cat /tmp/freebsd-update fetch
-env PAGER=cat /tmp/freebsd-update install
+env PAGER=cat /tmp/freebsd-update fetch install
+
 echo WITH_PKGNG=yes >> /etc/make.conf
 env ASSUME_ALWAYS_YES=YES pkg bootstrap
 pkg update
