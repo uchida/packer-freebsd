71e762898a1378b6b8540115dfe5ff00acf457a4
Akihiro Uchida <uchida@turbare.net>
2015-06-20 11:29:39 +0900

    use wildcard to match virtualbox-iso and vmware-vmx

--- scripts/vmguest.sh
+++ scripts/vmguest.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -ex
 case "$PACKER_BUILDER_TYPE" in
-  virtualbox-iso)
+  virtualbox-*)
     pkg install -y virtualbox-ose-additions
     sysrc vboxnet_enable="YES"
     sysrc vboxguest_enable="YES"
     sysrc vboxservice_enable="YES"
     ;;
-  vmware-iso)
+  vmware-*)
     pkg install -y open-vm-tools-nox11
     sysrc ifconfig_vxn0="dhcp"
     sysrc vmware_guest_vmblock_enable="YES"
