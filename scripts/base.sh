5e920dbfcf6a40f64353b0dde16498cfde562e23
Akihiro Uchida <uchida@turbare.net>
2015-08-29 20:25:36 +0900

    upgrade to 10.2-RELEASE

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -ex
 
-sed -e 's/\[ ! -t 0 \]/false/' /usr/sbin/freebsd-update > /tmp/freebsd-update
-chmod +x /tmp/freebsd-update
-env PAGER=cat /tmp/freebsd-update fetch install
+freebsd-update --not-running-from-cron fetch install
 
 echo WITH_PKGNG=yes >> /etc/make.conf
 env ASSUME_ALWAYS_YES=YES pkg bootstrap
