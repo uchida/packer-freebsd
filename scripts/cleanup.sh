e9e8969fb2e57e3da06619399d3dac2dbc40e285
Akihiro Uchida <uchida@turbare.net>
2015-09-13 23:42:06 +0900

    cleanup dhcp leases

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -6,6 +6,7 @@ rm -rf /var/db/freebsd-update/files
 mkdir /var/db/freebsd-update/files
 rm -f /var/db/freebsd-update/*-rollback
 rm -rf /var/db/freebsd-update/install.*
+rm -f /var/db/dhclient.leases.*
 rm -rf /boot/kernel.old
 rm -f /*.core
 rm -rf /tmp/*
