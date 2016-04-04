00d8d5ba33a5e12179a81639ad596e893505f648
Akihiro Uchida <uchida@turbare.net>
2016-04-04 21:12:14 +0900

    upgrade 10.3-RELEASE

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-freebsd-update --not-running-from-cron fetch install
+freebsd-update --not-running-from-cron fetch install || :
 
 echo WITH_PKGNG=yes >> /etc/make.conf
 env ASSUME_ALWAYS_YES=YES pkg bootstrap
