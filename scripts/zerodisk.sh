0303fa6fe608ff311215e1e7bd97fd81797dee23
Akihiro Uchida <uchida@turbare.net>
2016-05-05 10:18:01 +0900

    update scripts/zerodisk.sh for ufs

--- scripts/zerodisk.sh
+++ scripts/zerodisk.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -ex
-zfs create -o compression=off -o sync=standard -o mountpoint=/var/tmp zroot/empty
+if zfs list -H -o name zroot; then
+  zfs create -o compression=off -o sync=standard -o mountpoint=/var/tmp zroot/empty
+  trap 'zfs destroy zroot/empty' 0
+fi
 dd if=/dev/zero of=/var/tmp/EMPTY bs=1M || :
 rm /var/tmp/EMPTY
 sync
-zfs destroy zroot/empty
