416555ad2e7d1854217e38447797274095d236e6
Akihiro Uchida <uchida@turbare.net>
2016-05-07 19:58:56 +0900

    autoboot_delay=0 and pkg upgrade in scripts/base.sh

--- scripts/base.sh
+++ scripts/base.sh
@@ -6,3 +6,5 @@ freebsd-update --not-running-from-cron fetch install || :
 echo WITH_PKGNG=yes >> /etc/make.conf
 env ASSUME_ALWAYS_YES=YES pkg bootstrap
 pkg update
+pkg upgrade -y
+echo 'autoboot_delay="0"' >> /boot/loader.conf
